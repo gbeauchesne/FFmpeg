dec88085c03bc85ff59585ddf5733dc793082ec2
Gwenole Beauchesne <gwenole.beauchesne@intel.com>
2015-08-18 15:33:10 +0200

    hwaccel: add infrastructure to hold accelerator config options.
    The options are live from AVHWaccel.init() to AVHWAccel.uninit(). As such,
    they are specific to an active hwaccel and have a meaning to that hwaccel
    only during initialization. Options can be initialized by the user during
    AVCodecContext.get_format() with hwaccel-specific (public) helper functions.
    
    This is an internal infrastructure change/addition only.
    
    Signed-off-by: Gwenole Beauchesne <gwenole.beauchesne@intel.com>

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -1063,6 +1063,7 @@ int ff_get_format(AVCodecContext *avctx, const enum AVPixelFormat *fmt)
         if (avctx->hwaccel && avctx->hwaccel->uninit)
             avctx->hwaccel->uninit(avctx);
         av_freep(&avctx->internal->hwaccel_priv_data);
+        av_dict_free(&avctx->internal->hwaccel_config);
         avctx->hwaccel = NULL;
 
         ret = avctx->get_format(avctx, choices);
@@ -2520,6 +2521,7 @@ av_cold int avcodec_close(AVCodecContext *avctx)
         if (avctx->hwaccel && avctx->hwaccel->uninit)
             avctx->hwaccel->uninit(avctx);
         av_freep(&avctx->internal->hwaccel_priv_data);
+        av_dict_free(&avctx->internal->hwaccel_config);
 
         av_freep(&avctx->internal);
     }
